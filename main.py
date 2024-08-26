64d5b2e8fd8f4c2c0fda2e06e02e1f2e904fc214
ChristianKartveit <chrigk@stud.ntnu.no>
2024-08-26 12:05:30 +0200

    feat: Added date check for only todays trips, since ENTUR ATB keeps records trips over 4 days back?

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 import xml.etree.ElementTree as ET  # for parsing XML
 import numpy as np  # for using pandas
 import pandas as pd  # for using dataframes
-from datetime import datetime
+from datetime import datetime, date
 
 app = FastAPI()
 
@@ -15,15 +15,9 @@ def read_root():
     busID = res[0]["vehicle_ref"]
     if busID != "N/A":
         geo = getLocation(busID)
-        return {
-        "inbound": res[:2],
-        "geo": geo
-        }
-    return {
-        "inbound": res[:2]
-        }
+        return {"inbound": res[:2], "geo": geo}
+    return {"inbound": res[:2]}
     # print(res)
-    
 
 
 @app.get("/items/{item_id}")
@@ -34,7 +28,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def modifyApi():
     response = requests.get("https://api.entur.io/realtime/v1/rest/et?datasetId=ATB")
     if response.status_code == 200:
-        print("Yess")
+        # print("Yess")
         xml_data = response.content
 
         # Parsing the XML data with namespace
@@ -52,10 +46,10 @@ def modifyApi():
             lineref = estimated_journey.find("siri:LineRef", namespaces)
             vehicle_ref = estimated_journey.find("siri:VehicleRef", namespaces)
 
-            if vehicle_ref is not None:
-                print(f"VehicleRef found: {vehicle_ref.text}")
-            else:
-                print("VehicleRef not found")
+            # if vehicle_ref is not None:
+            #     print(f"VehicleRef found: {vehicle_ref.text}")
+            # else:
+            #     print("VehicleRef not found")
 
             if lineref is not None and lineref.text == "ATB:Line:2_22":
                 direction = estimated_journey.find("siri:DirectionRef", namespaces)
@@ -78,51 +72,55 @@ def modifyApi():
                             expectedArrivalTime = call.find(
                                 "siri:ExpectedArrivalTime", namespaces
                             )
-
                             # Check if the current stop is "Solsiden"
                             if stop_name.text == "Solsiden":
                                 arrivalStatus = call.find(
                                     "siri:ArrivalStatus", namespaces
                                 )
-
-                                aimed_arrival_time_formatted = datetime.fromisoformat(
-                                    aimedArrivalTime.text
-                                ).strftime("%H:%M:%S")
-                                expected_arrival_time_formatted = (
-                                    datetime.fromisoformat(
-                                        expectedArrivalTime.text
-                                    ).strftime("%H:%M:%S")
+                                expected_arrival_datetime = datetime.fromisoformat(
+                                    expectedArrivalTime.text
                                 )
+                                if expected_arrival_datetime.date() == date.today():
+                                    aimed_arrival_time_formatted = (
+                                        datetime.fromisoformat(
+                                            aimedArrivalTime.text
+                                        ).strftime("%H:%M:%S")
+                                    )
+                                    expected_arrival_time_formatted = (
+                                        datetime.fromisoformat(
+                                            expectedArrivalTime.text
+                                        ).strftime("%H:%M:%S")
+                                    )
 
-                                journey_data.append(
-                                    {
-                                        "stop_name": stop_name.text,
-                                        "arrival_status": arrivalStatus.text,
-                                        "aimed_arrival_time": aimed_arrival_time_formatted,
-                                        "expected_arrival_time": expected_arrival_time_formatted,
-                                        # "previous_stop_name": previous_stop_name,
-                                        # "previous_aimed_arrival_time": (
-                                        #     datetime.fromisoformat(
-                                        #         previous_aimed_arrival_time
-                                        #     ).strftime("%H:%M:%S")
-                                        #     if previous_aimed_arrival_time
-                                        #     else None
-                                        # ),
-                                        # "previous_expected_arrival_time": (
-                                        #     datetime.fromisoformat(
-                                        #         previous_expected_arrival_time
-                                        #     ).strftime("%H:%M:%S")
-                                        #     if previous_expected_arrival_time
-                                        #     else None
-                                        # ),
-                                        "expected_arrival_timestamp": expectedArrivalTime.text,
-                                        "vehicle_ref": (
-                                            vehicle_ref.text
-                                            if vehicle_ref is not None
-                                            else "N/A"
-                                        ),
-                                    }
-                                )
+                                    journey_data.append(
+                                        {
+                                            "stop_name": stop_name.text,
+                                            "arrival_status": arrivalStatus.text,
+                                            "aimed_arrival_time": aimed_arrival_time_formatted,
+                                            "expected_arrival_time": expected_arrival_time_formatted,
+                                            # "previous_stop_name": previous_stop_name,
+                                            # "previous_aimed_arrival_time": (
+                                            #     datetime.fromisoformat(
+                                            #         previous_aimed_arrival_time
+                                            #     ).strftime("%H:%M:%S")
+                                            #     if previous_aimed_arrival_time
+                                            #     else None
+                                            # ),
+                                            # "previous_expected_arrival_time": (
+                                            #     datetime.fromisoformat(
+                                            #         previous_expected_arrival_time
+                                            #     ).strftime("%H:%M:%S")
+                                            #     if previous_expected_arrival_time
+                                            #     else None
+                                            # ),
+                                            "expected_arrival_timestamp": expectedArrivalTime.text,
+                                            "vehicle_ref": (
+                                                vehicle_ref.text
+                                                if vehicle_ref is not None
+                                                else "N/A"
+                                            ),
+                                        }
+                                    )
 
                             # # Update previous stop information
                             # previous_stop_name = stop_name.text
@@ -148,26 +146,30 @@ def modifyApi():
 
 
 def getLocation(busID: str):
-    print(f"Looking for bus with ID: {busID}")
-    
+    # print(f"Looking for bus with ID: {busID}")
+
     # Fetch the vehicle monitoring data
     response = requests.get("https://api.entur.io/realtime/v1/rest/vm?datasetId=ATB")
-    
+
     if response.status_code == 200:
         xml_data = response.content
-        namespaces = {'siri': 'http://www.siri.org.uk/siri'}
+        namespaces = {"siri": "http://www.siri.org.uk/siri"}
         root = ET.fromstring(xml_data)
-        
+
         # Loop through all VehicleActivities
         for vehicle_activity in root.findall(".//siri:VehicleActivity", namespaces):
-            vehicle_ref = vehicle_activity.find(".//siri:VehicleMonitoringRef", namespaces)
-            
+            vehicle_ref = vehicle_activity.find(
+                ".//siri:VehicleMonitoringRef", namespaces
+            )
+
             if vehicle_ref is not None and vehicle_ref.text == busID:
-                vehicle_location = vehicle_activity.find(".//siri:VehicleLocation", namespaces)
+                vehicle_location = vehicle_activity.find(
+                    ".//siri:VehicleLocation", namespaces
+                )
                 if vehicle_location is not None:
                     latitude = vehicle_location.find("siri:Latitude", namespaces).text
                     longitude = vehicle_location.find("siri:Longitude", namespaces).text
                     return {"latitude": latitude, "longitude": longitude}
-    
+
     # If the busID is not found, return None
     return {"latitude": None, "longitude": None}
